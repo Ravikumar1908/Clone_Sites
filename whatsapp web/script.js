acc09b4efa7bdecfd079983589c19b5e279f774e
Ravi Kumar <132846098+Ravikumar1908@users.noreply.github.com>
2023-11-04 14:57:16 +0530

    Delete whatsapp web directory

--- whatsapp web/script.js
+++
@@ -1,6 +0,0 @@
-function messageadd()
-{
-    var a= document.getElementById("chatinput").value;
-    document.getElementById("shameekchat").innerHTML+='<p class="sendmessage">' + a +'</p>';
-    document.getElementById("chatinput").value="";
-}
