d78d3924ee6ead6ce38f339a9c75a69732999a30
Ravi Kumar <132846098+Ravikumar1908@users.noreply.github.com>
2024-11-28 08:57:06 +0530

    Update script.js revert

--- TextEditor/script.js
+++ TextEditor/script.js
@@ -15,7 +15,7 @@ eraseBtn.addEventListener("click",()=>{
 
 textArea.addEventListener("keydown",(e)=>{
     let count = parseInt(countBtn.textContent);
-    if(e.key=="Space")
+    if(e.key==" ")
     {
         countBtn.textContent= count+1;
     }
